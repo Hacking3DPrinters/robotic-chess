1d3d9a8e6e1021b3f723689961a954e350af333b
HippoProgrammer <bcgcustomerservices@gmail.com>
2024-06-11 15:26:21 +0000

    Fix TabError

--- gcode.py
+++ gcode.py
@@ -1,5 +1,5 @@
 # a simple module for parsing Python to .gcode
-class Parser(Object):
+class Parser:
     def __init__(self):
         self.cmd_list=[]
     def add_manual(self,cmd):
@@ -13,15 +13,13 @@ class Parser(Object):
             self.cmd_list.append('G91 ; set relative position')
         else:
             self.cmd_list.append('G90 ; set absolute position')
-            
-		if mm:
-			self.cmd_list.append('G21 ; mm')
+        if mm:
+            self.cmd_list.append('G21 ; mm')
         else:
-        	self.cmd_list.append('G20 ; inches')
-            
+            self.cmd_list.append('G20 ; inches')
         self.cmd_list.append('G00 X{x} Y{y} Z{z} ; go to set home'.format(x=str(home[0]), y=str(home[1]), z=str(home[2])))
     def add_movement(self,x=0,y=0,z=0,speed=200):
-        if x!=0 or y!=0 or z!=0:
+        if (x!=0 or y!=0 or z!=0):
             cmd='G01 '
             if x!=0:
                 cmd+='X{coordx} '.format(coordx=str(x))
@@ -31,7 +29,7 @@ class Parser(Object):
                 cmd+='Z{coordz} '.format(coordz=str(z))
             cmd+='F{f}'.format(f=str(speed))
             self.cmd_list.append(cmd)
-		else:
+        else:
         	pass
     def add_home(self):
         self.cmd_list.append('G00 X{x} Y{y} Z{z} ; go to set home'.format(x=str(self.home[0]), y=str(self.home[1]), z=str(self.home[2])))
@@ -44,7 +42,7 @@ class Parser(Object):
             self.cmd_list.append('G90 ; set absolute position')
     def change_mm(self,mm=True):
         if mm:
-			self.cmd_list.append('G21 ; mm')
+            self.cmd_list.append('G21 ; mm')
         else:
         	self.cmd_list.append('G20 ; inches')
     def change_home(self,home=[0,20,0]):
