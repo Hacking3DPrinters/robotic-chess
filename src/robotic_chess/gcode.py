0669e9470749e05d3623ba5f999f681eb4d606b5
HippoProgrammer <bcgcustomerservices@gmail.com>
2024-06-18 18:29:26 +0100

    Rewrite gcode library
    Make all functions 'return' instead of parsing to stdout in .gcode format

--- src/robotic_chess/gcode.py
+++ src/robotic_chess/gcode.py
@@ -1,26 +1,20 @@
 # a simple module for parsing Python to .gcode
-# NEEDS TO BE REWRITTEN
 class Parser:
-    def __init__(self):
-        self.cmd_list=[]
-    def add_manual(self,cmd):
-        # Warning: command may break Parser
-        self.cmd_list.add(str(cmd))
-    def setup(self,rel_pos=True,mm=True,home=[0,20,0]):
-        self.cmd_list.append('%')
+    def __init__(self,rel_pos=True,mm=True,home=[0,20,0]):
+        cmd_list=[]
         self.home=home
         self.pos=rel_pos
         if mm:
-            self.cmd_list.append('G21 ; mm')
+            cmd_list.append('G21 ; mm')
         else:
-            self.cmd_list.append('G20 ; inches')
-        self.cmd_list.append('G90 ; set absolute position for homing')
-        self.cmd_list.append('G00 X{x} Y{y} Z{z} ; go to set home'.format(x=str(home[0]), y=str(home[1]), z=str(home[2])))
+            cmd_list.append('G20 ; inches')
+        cmd_list.append('G90 ; set absolute position for homing')
+        cmd_list.append('G00 X{x} Y{y} Z{z} ; go to set home'.format(x=str(home[0]), y=str(home[1]), z=str(home[2])))
         if self.pos:
-            self.cmd_list.append('G91 ; set relative position')
+            cmd_list.append('G91 ; set relative position')
         else:
-            self.cmd_list.append('G90 ; set absolute position')
-       
+            cmd_list.append('G90 ; set absolute position')
+       return cmd_list
     def add_movement(self,x=0,y=0,z=0,speed=200):
         if (x!=0 or y!=0 or z!=0):
             cmd='G01 '
@@ -31,60 +25,36 @@ class Parser:
             if z!=0:
                 cmd+='Z{coordz} '.format(coordz=str(z))
             cmd+='F{f}'.format(f=str(speed))
-            self.cmd_list.append(cmd)
+            return [cmd]
         else:
         	pass
     def add_home(self):
+        cmd_list=[]
         old_pos=self.pos
-        self.change_pos(rel_pos=False)
-        self.cmd_list.append('G00 X{x} Y{y} Z{z} ; go to set home'.format(x=str(self.home[0]), y=str(self.home[1]), z=str(self.home[2])))
-        self.change_pos(old_pos)
+        cmd_list.append(self.change_pos(rel_pos=False))
+        cmd_list.append('G00 X{x} Y{y} Z{z} ; go to set home'.format(x=str(self.home[0]), y=str(self.home[1]), z=str(self.home[2])))
+        cmd_list.append(self.change_pos(old_pos))
+        return cmd_list
     def add_fan(self,speed=255):
         if speed==255:
-            self.cmd_list.append('M106 ; use fan')
+            return ['M106 ; use fan']
         elif speed==0:
-            self.cmd_list.append('M107 ; use fan')
+            return ['M107 ; use fan']
         else:
-            self.cmd_list.append('M106 S{s} ; use fan'.format(s=str(speed)))
+            return ['M106 S{s} ; use fan'.format(s=str(speed))]
     def change_pos(self,rel_pos=True):
         self.pos=rel_pos
         if self.pos:
-            self.cmd_list.append('G91 ; set relative position')
+            return ['G91 ; set relative position']
         else:
-            self.cmd_list.append('G90 ; set absolute position')
+            return ['G90 ; set absolute position']
     def change_mm(self,mm=True):
         if mm:
-            self.cmd_list.append('G21 ; mm')
+            return ['G21 ; mm']
         else:
-        	self.cmd_list.append('G20 ; inches')
+        	return ['G20 ; inches']
     def change_home(self,home=[0,20,0]):
         self.home=home
-    def parse(self, target=''):
-        if self.cmd_list[len(self.cmd_list)-1]!='%':
-            self.cmd_list.append('%')
-        if len(target)>0:
-            if target=='stdout':
-                output=''
-                for cmd in self.cmd_list:
-                    print('Parsing '+cmd+'...')
-                    output+=cmd+'\n'
-                return output
-            else:
-                f=open(target,'w')
-                for cmd in self.cmd_list:
-                    print('Parsing '+cmd+'...')
-                    f.write(cmd+'\n')
-                    f.close()
-        
-        else:
-            f=open('output.gcode','w')
-            for cmd in self.cmd_list:
-                print('Parsing '+cmd+'...')
-                f.write(cmd+'\n')
-                f.close()
-    def clear(self):
-        self.cmd_list=[]
-        
         
                 
         
