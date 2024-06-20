3f83924c62b199b1ea4ac2938df75dd6c6c5808a
HippoProgrammer <bcgcustomerservices@gmail.com>
2024-06-20 16:20:38 +0100

    Update __init__.py

--- src/robotic_chess/__init__.py
+++ src/robotic_chess/__init__.py
@@ -24,33 +24,48 @@ def letter_to_number(letter): # convert chess columns (a-h) to numbers (1-8)
 def notation_to_coords(move='a1a2'): # use complex function to convert a square-square move to [[coordx,coordy],[coordx,coordy]] format
   return [[1.59375+((letter_to_number(move[0:1])-1)*3.1875),1.59375+((int(move[1:2])-1)*3.1875)],[1.59375+((letter_to_number(move[2:3])-1)*3.1875),1.59375+((int(move[3:4])-1)*3.1875)]]
 
+def human_move():
+  while True:
+    		h_move = str(input('Input move: '))
+    		if len(h_move)==4:
+      		break
+      print('Input invalid. Should be formatted as [start][end], 	e.g. a1a2 for square a1 to square a2')
+    return h_move
+
+def computer_move(board):
+  return board.engine_move()
+
 b = Board() # initialise classes
 p = Parser()
 printer = robotic_chess.octoprint.Printer()
+
+piece_height = 100 # define piece height in mm
+
+printer.run_gcode(p.setup(rel_pos=False))
 
 playing = True # track game state
 mode = 0 # track game mode (0 = human vs. computer, 1 = human vs. human-controlled computer, 2 = computer vs. computer) - functionality will be added later
 
 while playing: # while game is ongoing
   if mode!=2: # if human is playing
-    while True:
-    		h_move = str(input('Input move: '))
-    		if len(h_move)==4:
-      		break
-      print('Input invalid. Should be formatted as [start][end], e.g. a1a2 for square a1 to square a2')
-    b.opponent_move(moves=[h_move])
+    b.opponent_move(human_move())
   else: # if computer vs computer
     # take move from stockfish (see below)
     pass # placeholder
   # check for win after each move
   if mode!=1: # if computer is playing
-    # take move from stockfish
-    # input move to stockfish
-    # get best move from stockfish
-    # use notation_to_coords to parse
-    # send to gcodelib to convert to gcode
-    # send to octoprint to run
-    pass # placeholder
+    best_move = computer_move(b)
+    best_move_coords = notation_to_coords(move=best_move)
+    if get_capture(best_move):
+      capture_coords={'x':best_move_coords[0][0],'y':best_move_coords[0][1],'z':piece_height}
+      printer.run_gcode(p.add_movement(z=20, speed=1000))
+      printer.run_gcode(p.add_movement(x=capture_coords['x'], y=capture_coords['y'], z=capture_coords['z'], speed=500))
+      printer.run_gcode(p.add_fan())
+      printer.run_gcode(p.add_movement(z=20, speed=1000))
+      # move to bin
+      printer.run_gcode(p.add_fan(speed=0))
+      printer.run_gcode(p.add_home())
+    # make move
   else: # if human vs human
     # take move from remote human (for future version v2)
     pass # placeholder
