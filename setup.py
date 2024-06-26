59077524cab255c623840946769ac17f3e42fadb
HippoProgrammer <bcgcustomerservices@gmail.com>
2024-06-26 16:31:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ from os import system
 if __name__ == "__main__":
   setup()
   system('git clone https://github.com/official-stockfish/Stockfish.git')
-  system('cd Stockfish/src/')
-  system('make -j build')
-  system('sudo mv ./stockfish /usr/local/bin/')
+  system('cd Stockfish/src/ && make -j build')
+  system('sudo mv Stockfish/src/stockfish /usr/local/bin/')
   system('sudo chmod a+x /usr/local/bin/stockfish')
   
