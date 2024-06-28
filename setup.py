0f286b0d013d4b251aa0535d8c4259b25f5bf733
HippoProgrammer <bcgcustomerservices@gmail.com>
2024-06-28 07:27:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import setup
 from os import system
+import platform
+import getpass
 
 if __name__ == "__main__":
-  setup()
-  system('git clone https://github.com/official-stockfish/Stockfish.git')
-  system('cd Stockfish/src/ && make -j build')
-  system('sudo mv Stockfish/src/stockfish /usr/local/bin/')
-  system('sudo chmod a+x /usr/local/bin/stockfish')
-  
+  if platform.system()=='Linux':
+    setup()
+    system('git clone https://github.com/official-stockfish/Stockfish.git')
+    system('cd Stockfish/src/ && make -j build')
+    system('sudo mv Stockfish/src/stockfish /usr/local/bin/')
+    system('sudo chmod a+x /usr/local/bin/stockfish')
+  elif platform.system()=='Windows':
+    setup()
+    system('git clone https://github.com/official-stockfish/Stockfish.git')
+    system('cd Stockfish/src/ && make -j build')
+    system('move Stockfish/src/stockfish C:/Users/'+str(getpass.getuser())+'/')
+  else:
+    raise OSError('OS unsupported.')
